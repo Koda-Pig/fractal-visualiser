666e4fbf8951a3c235d9196510bc60e90a630da0
Josh Koter <josh.koter@gmail.com>
2022-12-09 19:59:45 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+import './style.scss'
+
+/*
+const canvas = document.querySelector('canvas'),
+  ctx = canvas.getContext('2d'),
+  fract1 = document.querySelector('#fract1')
+
+canvas.width = window.innerWidth
+canvas.height = window.innerHeight
+
+function animate(image, size) {
+  ctx.save()
+
+  // ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.drawImage(
+    image,
+    canvas.width / 2 - size / 2,
+    canvas.height / 2 - size / 2,
+    size,
+    size
+  )
+
+  ctx.translate(canvas.width / 2 - size / 2, canvas.height / 2 - size / 2)
+
+  // // rotate context by 45 degrees counter clockwise
+  ctx.rotate(2 * Math.PI)
+  ctx.drawImage(
+    image,
+    canvas.width / 2 - size / 2,
+    canvas.height / 2 - size / 2,
+    size,
+    size
+  )
+
+  ctx.restore()
+}
+
+animate(fract1, 500)
+*/
